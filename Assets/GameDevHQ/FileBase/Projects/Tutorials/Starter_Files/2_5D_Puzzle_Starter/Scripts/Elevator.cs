5302c5f221815931deb567003fbf86609eadabdc
DragonEagle <allensoard@yahoo.com>
2020-11-24 02:08:37 -0800

    Finished puzzle challanges

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/Elevator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _targetA, _targetB;
+    [SerializeField]
+    private float _speed = 1.0f;
+
+    private bool goingDown = false;
+
+    // Call elevator
+    public void CallElevator()
+    {
+        goingDown = !goingDown;
+    }
+    private void FixedUpdate()
+    {
+        if (goingDown)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetA.position, _speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetB.position, _speed * Time.deltaTime);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = this.transform;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+        }
+    }
+}
--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/Player.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Puzzle_Starter/Scripts/Player.cs
@@ -12,6 +12,9 @@ public class Player : MonoBehaviour
     private float _gravity = 1.0f;
     [SerializeField]
     private float _jumpHeight = 15.0f;
+    [SerializeField]
+    private float _pushPower = 2.0f;
+
     private float _yVelocity;
     private bool _canDoubleJump = false;
     [SerializeField]
@@ -19,6 +22,12 @@ public class Player : MonoBehaviour
     private UIManager _uiManager;
     [SerializeField]
     private int _lives = 3;
+
+    private Vector3 _direction, _velocity;
+    private bool _canWallJump = false;
+    private Vector3 _wallJumpDirection;
+
+    public int Coins { get { return _coins; } }
 
     // Start is called before the first frame update
     void Start()
@@ -38,11 +47,11 @@ public class Player : MonoBehaviour
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        Vector3 direction = new Vector3(horizontalInput, 0, 0);
-        Vector3 velocity = direction * _speed;
 
         if (_controller.isGrounded == true)
         {
+            _direction = new Vector3(horizontalInput, 0, 0);
+            _velocity = _direction * _speed;
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 _yVelocity = _jumpHeight;
@@ -51,7 +60,7 @@ public class Player : MonoBehaviour
         }
         else
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(KeyCode.Space) && _canWallJump == false)
             {
                 if (_canDoubleJump == true)
                 {
@@ -59,13 +68,18 @@ public class Player : MonoBehaviour
                     _canDoubleJump = false;
                 }
             }
+            if (Input.GetKeyDown(KeyCode.Space) && _canWallJump)
+            {
+                _velocity = _wallJumpDirection * _speed;
+                _yVelocity = _jumpHeight;
+            }
 
             _yVelocity -= _gravity;
         }
 
-        velocity.y = _yVelocity;
+        _velocity.y = _yVelocity;
 
-        _controller.Move(velocity * Time.deltaTime);
+        _controller.Move(_velocity * Time.deltaTime);
     }
 
     public void AddCoins()
@@ -84,6 +98,23 @@ public class Player : MonoBehaviour
         if (_lives < 1)
         {
             SceneManager.LoadScene(0);
+        }
+    }
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        _canWallJump = false;
+        if(!_controller.isGrounded && hit.transform.CompareTag("Wall")){
+            _canWallJump = true;
+            _wallJumpDirection = hit.normal;
+        }
+        else if (hit.transform.CompareTag("MovingBox"))
+        {
+            Rigidbody body = hit.transform.GetComponent<Rigidbody>();
+            if (body)
+            {
+                Vector3 pushDir = new Vector3(hit.moveDirection.x, 0, 0);
+                body.velocity = pushDir * _pushPower;
+            }
         }
     }
 }
