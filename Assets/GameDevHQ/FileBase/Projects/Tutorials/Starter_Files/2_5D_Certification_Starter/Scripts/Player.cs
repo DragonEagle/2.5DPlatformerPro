b8860090fb6c80fac2a1d67d943a66c301bd7d71
DragonEagle <allensoard@yahoo.com>
2020-11-26 04:00:54 -0800

    Setup character animation

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
@@ -12,11 +12,15 @@ public class Player : MonoBehaviour
     private float _gravity = 1.0f;
     private Vector3 moveDirection;
     CharacterController _controller;
+    private Animator _anim;
+
+    private bool _jumping = false;
 
     // Start is called before the first frame update
     void Start()
     {
         _controller = GetComponent<CharacterController>();
+        _anim = GetComponentInChildren<Animator>();
         moveDirection = new Vector3(0, 0, 0);
     }
 
@@ -24,12 +28,27 @@ public class Player : MonoBehaviour
     void Update()
     {
         float yVelocity = moveDirection.y;
-        float horizontalInput = Input.GetAxis("Horizontal");
         if (_controller.isGrounded)
         {
+            if (_jumping)
+            {
+                _jumping = false;
+                _anim.SetBool("IsJumping", _jumping);
+            }
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            if ((transform.forward.z < 0 && horizontalInput > 0) || (transform.forward.z > 0 && horizontalInput < 0))
+            {
+                transform.Rotate(new Vector3(0,180,0));
+            }
+            if (_anim)
+            {
+                _anim.SetFloat("Speed", Mathf.Abs(horizontalInput));
+            }
             moveDirection = new Vector3(0, 0, horizontalInput * _speed);
             if (Input.GetButtonDown("Jump"))
             {
+                _jumping = true;
+                _anim.SetBool("IsJumping", _jumping);
                 yVelocity = _jumpHeight;
             } 
         } else
@@ -38,8 +57,5 @@ public class Player : MonoBehaviour
         }
         moveDirection.y = yVelocity;
         _controller.Move(moveDirection * Time.deltaTime);
-        // if grounded
-        // calculate direction based on input
-        // or jump
     }
 }
