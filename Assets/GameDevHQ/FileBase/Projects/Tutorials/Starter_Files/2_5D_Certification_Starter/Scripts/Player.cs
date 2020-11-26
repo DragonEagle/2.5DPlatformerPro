6e41753fe902b75f7d065a77cf5d38dcd73e868d
DragonEagle <allensoard@yahoo.com>
2020-11-26 02:33:46 -0800

    Added character controller for player

---
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private float _jumpHeight = 15.0f;
+    [SerializeField]
+    private float _gravity = 1.0f;
+    private Vector3 moveDirection;
+    CharacterController _controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        moveDirection = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yVelocity = moveDirection.y;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        if (_controller.isGrounded)
+        {
+            moveDirection = new Vector3(0, 0, horizontalInput * _speed);
+            if (Input.GetButtonDown("Jump"))
+            {
+                yVelocity = _jumpHeight;
+            } 
+        } else
+        {
+            yVelocity -= _gravity;
+        }
+        moveDirection.y = yVelocity;
+        _controller.Move(moveDirection * Time.deltaTime);
+        // if grounded
+        // calculate direction based on input
+        // or jump
+    }
+}
